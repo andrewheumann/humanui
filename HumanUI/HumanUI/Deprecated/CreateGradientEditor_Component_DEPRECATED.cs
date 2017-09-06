967e41d0caf3e49d78f5ec2bba461dcb0ee1d50d
Andrew Heumann <andheum@gmail.com>
2017-09-06 12:44:48 -0400

    Modifications to gradient editor based on PR by @NicolasChaulet
    added ability to set default

--- HumanUI/HumanUI/Deprecated/CreateGradientEditor_Component_DEPRECATED.cs
+++ HumanUI/HumanUI/Deprecated/CreateGradientEditor_Component_DEPRECATED.cs
@@ -11,12 +11,12 @@ using Grasshopper.Kernel.Special;
 
 namespace HumanUI.Components
 {
-    public class CreateGradientEditor_Component : GH_Component
+    public class CreateGradientEditor_Component_DEPRECATED : GH_Component
     {
         /// <summary>
         /// Initializes a new instance of the CreateGradientEditor_Component class.
         /// </summary>
-        public CreateGradientEditor_Component()
+        public CreateGradientEditor_Component_DEPRECATED()
           : base("Create Gradient Editor", "Gradient",
               "Creates an editable gradient in the UI",
               "Human UI", "UI Elements")
@@ -28,10 +28,8 @@ namespace HumanUI.Components
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            pManager.AddGenericParameter("Gradient(s)", "G", "The Gradient or collection of gradient presets to include. Multiples will be ignored if the preset menu is not showing.", GH_ParamAccess.list);
+            pManager.AddGenericParameter("Gradient(s)", "G", "The Gradient or collection of gradient presets to include. The first one will be the default. Multiples will be ignored if the preset menu is not showing.", GH_ParamAccess.list);
             pManager[0].Optional = true;
-            pManager.AddGenericParameter("Default Gradient", "D", "The default gradient to show - only used if the preset menu is enabled.", GH_ParamAccess.list);
-            pManager[1].Optional = true;
             pManager.AddBooleanParameter("Show Editor", "E", "Set to true to show an interactive gradient editor", GH_ParamAccess.item, true);
             pManager.AddBooleanParameter("Show Presets", "P", "Set to true to show a pulldown menu of gradient presets", GH_ParamAccess.item, false);
         }
@@ -51,56 +49,41 @@ namespace HumanUI.Components
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             List<GH_Gradient> gradients = new List<GH_Gradient>();
-            List<GH_Gradient> defaultGradient = new List<GH_Gradient>();
             List<object> gradientObjects = new List<object>();
-            List<object> defaultGradientObjects = new List<object>();
             bool showPresets = false;
             bool showEditor = true;
             DA.GetData("Show Presets", ref showPresets);
             DA.GetData("Show Editor", ref showEditor);
 
             DA.GetDataList("Gradient(s)", gradientObjects);
-            DA.GetDataList("Default Gradient", defaultGradientObjects);
-            var hasDefault = Params.Input[1].SourceCount > 0;
-            ExtractGradients(gradients, gradientObjects);
+
+            if(gradientObjects.Count > 0 && gradientObjects[0] is GH_ObjectWrapper)
+            {
+                foreach(GH_ObjectWrapper wrapper in gradientObjects.OfType<GH_ObjectWrapper>())
+                {
+                    GH_GradientControl editor = wrapper.Value as GH_GradientControl;
+                    gradients.Add(editor.Gradient);
+                }
+            } else
+            {
+                var doc = OnPingDocument();
+                doc.Objects.OfType<GH_GradientControl>().Where(ao => Params.Input[0].DependsOn(ao)).ToList().ForEach(g => gradients.Add(g.Gradient));
+            }
 
             List<HUI_Gradient> presets = new List<HUI_Gradient>();
 
-            if (hasDefault)
+            foreach(GH_Gradient g in gradients)
             {
-                ExtractGradients(defaultGradient, defaultGradientObjects,1);
-                presets.Add(HUI_Gradient.FromGHGradient(defaultGradient.First()));
-            }
-
-            foreach (GH_Gradient g in gradients)
-            {
-                //w/ addition of the default value, have to make sure we don't double add a gradient
-                var gradientToAdd = HUI_Gradient.FromGHGradient(g);
-                if (presets.Select(p => p.ToString()).Any(p => p.Contains(gradientToAdd.ToString()))) continue;
-                presets.Add(gradientToAdd);
+                presets.Add(HUI_Gradient.FromGHGradient(g));
             }
             HUI_GradientEditor hge = new HUI_GradientEditor(showPresets, showEditor, presets);
             DA.SetData("Gradient Editor", new UIElement_Goo(hge, "Gradient Editor", InstanceGuid, DA.Iteration));
 
         }
 
-        private void ExtractGradients(List<GH_Gradient> gradients, List<object> gradientObjects, int paramIndex = 0)
-        {
-            if (gradientObjects.Count > 0 && gradientObjects[0] is GH_ObjectWrapper)
-            {
-                foreach (GH_ObjectWrapper wrapper in gradientObjects.OfType<GH_ObjectWrapper>())
-                {
-                    GH_GradientControl editor = wrapper.Value as GH_GradientControl;
-                    gradients.Add(editor.Gradient);
-                }
-            }
-            else
-            {
-                var doc = OnPingDocument();
-                doc.Objects.OfType<GH_GradientControl>().Where(ao => Params.Input[paramIndex].DependsOn(ao)).ToList().ForEach(g => gradients.Add(g.Gradient));
-            }
-        }
+        public override GH_Exposure Exposure => GH_Exposure.hidden;
 
+        public override bool Obsolete => true;
         /// <summary>
         /// Provides an Icon for the component.
         /// </summary>
@@ -109,6 +92,6 @@ namespace HumanUI.Components
         /// <summary>
         /// Gets the unique ID for this component. Do not change this ID after release.
         /// </summary>
-        public override Guid ComponentGuid => new Guid("{DA8DD6A3-AD57-471E-B891-94902C8647ED}");
+        public override Guid ComponentGuid => new Guid("{818ce7bb-0e46-4203-bc92-419bb786ba49}");
     }
 }
