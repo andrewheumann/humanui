af81810a9b782eb7e87223726233081a0003ff36
Andrew Heumann <andrew.heumann@wework.com>
2017-10-17 15:42:23 -0400

    version bump

--- HumanUI/HumanUI/Properties/Resources.Designer.cs
+++ HumanUI/HumanUI/Properties/Resources.Designer.cs
@@ -421,7 +421,7 @@ namespace HumanUI.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0.8.0.0.
+        ///   Looks up a localized string similar to 0.8.1.0.
         /// </summary>
         internal static string CURRENT_VERSION {
             get {
