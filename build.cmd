ebfc25f85cf3fc7cbadc447da33310b9b6d17b95
Andrew Heumann <andrew@heumann.com>
2020-04-30 18:55:56 -0400

    more stuff!!!!!!!!!!!!!!!!!!!!!!

---
+++ build.cmd
@@ -0,0 +1,5 @@
+cd "C:\users\andrew\documents\repos\humanui\dist"
+"C:\Program Files\Rhino WIP\System\Yak.exe" build
+ren human-ui-0.8.5-rh5_1-win.yak human-ui-0.8.5-any-win.yak
+"C:\Program Files\Rhino WIP\System\Yak.exe" push human-ui-0.8.5-any-win.yak
+"C:\Program Files\Rhino WIP\System\Yak.exe" install human-ui
